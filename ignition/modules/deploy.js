466bcd0f2ae74da440b8cd8784bde625a5bdea8c
kumar Devashish <kumardevashish000@gmail.com>
2024-06-25 02:04:40 +0530

    chore: Update dependencies and add deployment script
    - Update package.json to include new dependencies: @nomicfoundation/solidity-analyzer and ethers
    - Add deployment script in deploy.js to deploy the "Frontend" contract
    - Connect wallet automatically on page load in App.jsx

---
+++ ignition/modules/deploy.js
@@ -0,0 +1,18 @@
+const main = async () => {
+    const contractFactory = await ethers.getContractFactory("Frontend");
+    const contract = await contractFactory.deploy();
+
+    console.log("Contract deployed to:", contract.target);
+};
+
+const runMain=async()=>{
+    try{
+        await main();
+        process.exit(0);
+    } catch(error){
+        console.log(error);
+        process.exit(1);
+    };
+};
+
+runMain();
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
+import { useState, useEffect} from 'react';
 import './App.css'
+import abi from '../artifacts/contracts/Frontend.sol/Frontend.json'
 
 function App() {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+        
+      });
+      setCurrentAccount(accounts[0]);
   
+      console.log("Connected:", currentAccount);
+      // ethereum.on('chainChanged', (_chainId: string) => window.location.reload());
+      // setCorrectNetwork(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(()=>{
+    connectWallet();
+  },[])
   return (
     <div className='home'>
-      <h1>BlockChain voting project</h1>
+      <h1>Function Frontend</h1>
       <p>
-        This is a project to demonstrate the use of blockchain technology in voting.
+        This is a project to demonstrate the use of blockchain technology.
         </p>
+        <h3>{currentAccount}</h3>
         <div className='button-div'>
           <div className='operator'>-</div>
           <div className='operator'>0</div>
